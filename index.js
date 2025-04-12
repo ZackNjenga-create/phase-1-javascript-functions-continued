d91683ad682286b2a1562ab2f7f004b0be26c6b5
Zack Njenga <izackkiari@gmail.com>
2025-04-12 14:32:15 +0300

    function continue

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // code your solution here
+
+razzle()
+function razzle(lawyer = "Billy", target = "'em"){
+    console.log(`${lawyer} razzle-dazzles ${target}!`);
+}
+
+// const button = document.getElementById("button")
+// button.addEventListener("click", function (){
+//     console.log("Yet more razzling")
+// })
+// const fn = function (){
+//         console.log("Yet more razzling");
+//     };
+function outer(greeting, msg = "It's a fine day to learn"){
+    const innerFunction = function(name, lang="Pyhton"){
+        return `${greeting}, ${name}, ${msg} ${lang}`
+    }
+return innerFunction("student", "Javascript")
+}
+outer("Hello")
+(function (baseNumber){
+    return baseNumber + 3;
+})(2);
+
+(function(){
+    console.log("Yet more razzling");
+});
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+
+function mondayWork(activity = "go to the office"){
+   return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrap = "*"){
+    return function (adjective = "special"){
+        return `You are ${wrap}${adjective}${wrap}!`;
+    }
+    
+}
